a2fa536e427864c5445e4d708b308a3841bb3ddb
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2021-09-21 01:16:33 +0100

    Fix issues

--- components/SeoHead.jsx
+++ components/SeoHead.jsx
@@ -4,12 +4,12 @@ export default function SeoHead({ siteTitle, description, keywords }) {
 
   return (
     <Head>
-      <title>{`PunhubCentral | ${siteTitle}`}</title>
+      <title>{`DataMax UI | ${siteTitle}`}</title>
       <link rel="icon" href="/favicon.ico" />
       <meta name="description" content={`${description}`}/>
       <meta 
         name="keywords" 
-        content={`Punhub, Central, Puns, HipHop, Pop, Rap, ${keywords}`} 
+        content={`${keywords}`} 
       />
       <meta name="author" content="Elisha Temiloluwa a.k.a TemmyScope" />
       <meta name="robots" content="index, follow" />
